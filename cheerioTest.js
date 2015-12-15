d18be39772d3f4efb1eb4d95cd8da253ac88f418
skultimate5 <sck42@case.edu>
2015-12-15 16:29:58 -0500

    Count total number of articles

--- cheerioTest.js
+++ cheerioTest.js
@@ -1,18 +1,20 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-var searchTermOverall = '?s=+hot+metal';
+var searchTermOverall = '?s=+sophie+knowles';
 //var url = 'http://www.ultiworld.com/?s=+' + searchTerm;
 var url = "http://www.ultiworld.com/";
 //var url = "http://www.ultiworld.com/page/3/";
 
 var count = 1;
+var numArticles = 0;
 
 searching(url, searchTermOverall, count);
 
 function searching(url, searchTerm, count){
 	getOnePageArticles(url, searchTerm, count, function(length){
-		console.log(count + " numArticles: " + length);
+		numArticles = numArticles + length;
+		console.log(count + " numArticles: " + length + "\nTotal numArticles: " + numArticles);
 		if (length == 10){
 			count++
 			if (count > 1){
@@ -26,13 +28,24 @@ function searching(url, searchTerm, count){
 
 function getOnePageArticles(url, searchTerm, count, callback){
 	request(url + "page/" + count + "/" + searchTerm, function(err, resp, body){
-	  console.log(url + "page/" + count + "/" + searchTerm)
-	  $ = cheerio.load(body);
-	  var listElements = $('.snippet-excerpt__heading'); //use your CSS selector here
-	  //console.log(listElements);
-	  /*$(listElements).each(function(i, list){
-	    console.log($(list).text());
-	  });*/
-	  callback(listElements.length);
+
+		//Check for error
+	    if(err){
+	        return console.log('Error:', err);
+	    }
+
+	    //Check for right status code
+	    if(resp.statusCode !== 200){
+	        return console.log('Invalid Status Code Returned:', resp.statusCode);
+	    }
+
+		  console.log(url + "page/" + count + "/" + searchTerm)
+		  $ = cheerio.load(body);
+		  var listElements = $('.snippet-excerpt__heading'); //use your CSS selector here
+		  //console.log(listElements);
+		  /*$(listElements).each(function(i, list){
+		    console.log($(list).text());
+		  });*/
+		  callback(listElements.length);
 	});
 }
