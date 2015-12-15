14d9b9162f315c93025678583c0305b42254a29b
skultimate5 <sck42@case.edu>
2015-12-15 16:08:45 -0500

    Fixed search term so doesn't add same part to url over and over

--- cheerioTest.js
+++ cheerioTest.js
@@ -1,35 +1,38 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-var searchTerm = '?s=+hot+metal';
+var searchTermOverall = '?s=+hot+metal';
 //var url = 'http://www.ultiworld.com/?s=+' + searchTerm;
-//var url = "http://www.ultiworld.com/";
-var url = "http://www.ultiworld.com/page/3/";
+var url = "http://www.ultiworld.com/";
+//var url = "http://www.ultiworld.com/page/3/";
 
 var count = 1;
 
-searching(url, searchTerm);
+searching(url, searchTermOverall, count);
 
-function searching(url, searchTerm){
-	getOnePageArticles(url, searchTerm, function(length){
-		console.log(count + "numArticles: " + length);
+function searching(url, searchTerm, count){
+	getOnePageArticles(url, searchTerm, count, function(length){
+		console.log(count + " numArticles: " + length);
 		if (length == 10){
 			count++
 			if (count > 1){
-				searching(url, "page/" + count + "/" + searchTerm)
+				console.log("Searching page " + count + "...");
+				searching(url, searchTerm, count)
 			}
 		}
 	});
 }
 
 
-function getOnePageArticles(url, searchTerm, callback){
-	request(url + searchTerm, function(err, resp, body){
+function getOnePageArticles(url, searchTerm, count, callback){
+	request(url + "page/" + count + "/" + searchTerm, function(err, resp, body){
+	  console.log(url + "page/" + count + "/" + searchTerm)
 	  $ = cheerio.load(body);
-	  listElements = $('.snippet-excerpt__heading'); //use your CSS selector here
-	  $(listElements).each(function(i, list){
+	  var listElements = $('.snippet-excerpt__heading'); //use your CSS selector here
+	  //console.log(listElements);
+	  /*$(listElements).each(function(i, list){
 	    console.log($(list).text());
-	  });
+	  });*/
 	  callback(listElements.length);
 	});
 }
