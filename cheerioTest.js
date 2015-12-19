d575a55a1328a6e115b5f0364c402e7f80bbd202
skultimate5 <sck42@case.edu>
2015-12-19 00:35:34 -0500

    Now runs by redirecting to same page until all pages have been searched

--- cheerioTest.js
+++ cheerioTest.js
@@ -15,100 +15,86 @@ app.get('/', function(request, response) {
   response.render('index', {
   	numArticles: null,
   	numArticlesYear: null,
-	year: null
+	year: null,
+	searchVal: null,
+	count: 1,
+	numArticlesTotal: 0
   });
 });
 
-app.get('/myForm', function(req, response){
-	var searchVal = req.query.name;
+app.get('/search/:count/:searchVal/:numArticlesTotal', function(req, response){
+	var count = req.params.count.split(":")[1];
+	console.log(req.params);
+	if (count == 1){
+		var searchVal = req.query.name;
+	}
+	else{
+		var searchVal = req.params.searchVal.split(":")[1];
+	}
+	var numArticlesTotal = req.params.numArticlesTotal.split(":")[1];
 	console.log("Search Value is: " + searchVal)
 	var formattedSearchVal = formatSearchVal(searchVal);
-	var url = "http://www.ultiworld.com/";
 	var year = 2014;
-	var count = 1;
 	var numArticles = 0;
 	var numArticlesYear = 0;
-	var searchVal2 = formattedSearchVal;
 	var doneBoolean = false;
 
-	searching(url, searchVal2, count, year);
-
-	function searching(url, searchTerm, count, year, doneBoolean){
-		getOnePageArticles(url, searchTerm, count, year, doneBoolean, function(length, certainYearArticles, doneBoolean){
-			numArticles = numArticles + length;
-			console.log(count + " numArticles: " + length + "\nTotal numArticles: " + numArticles);
-			numArticlesYear = numArticlesYear + certainYearArticles.length;
-			console.log(count + " " + year + " articles: " + certainYearArticles.length + "\nTotal numArticlesYear: " + numArticlesYear);
-			for (var i = 0; i < certainYearArticles.length; i++){
-				console.log(certainYearArticles[i])
-			}
-			if (!doneBoolean){
-				count++
-				if (count > 1){
-					console.log("Searching page " + count + "...");
-					searching(url, searchTerm, count, year)
-				}
-			}
-			else{
-				console.log("Done Searching")
-				console.log(numArticles)
-				response.render('index', {
-			 		numArticles: numArticles,
-			 		numArticlesYear: numArticlesYear,
-			 		year: year
-				});
-			}
-		});
-	}
-
-
-	function getOnePageArticles(url, searchTerm, count, year, doneBoolean, callback){
-		request(url + "page/" + count + "/" + searchTerm, function(err, resp, body){
-
-			//Check for error
-		    if(err){
-		        return console.log('Error:', err);
-		    }
-
-		    //Check for right status code
-		    if(resp.statusCode !== 200){
-		        return console.log('Invalid Status Code Returned:', resp.statusCode);
-		    }
-
-			  console.log(url + "page/" + count + "/" + searchTerm)
-			  $ = cheerio.load(body);
-			  var previousPageButton = $('.paging-nav__previous');
-			  if (!previousPageButton[0]){
-			  	doneBoolean = true;
-			  }			  
-			  var listElements = $('.snippet-excerpt__heading'); 		//.text() for each element in this gives you the name of each article
-			  var datesPosted = $('.snippet-excerpt__byline');
-			  var certainYearArticles = [];
-			  $(datesPosted).each(function(i, date){
-			  	var dateHTML = $(date).html();
-			  	var substring = dateHTML.substring(0,17).split(" ")[2]		//getting year of article
-			  	if (substring == '' + year){
-			  		certainYearArticles.push($(listElements[i]).text());
-			  	}
-			  });
-			  callback(listElements.length, certainYearArticles, doneBoolean);
-		});
-	}
-});
-
-app.get('/searched', function(request, response) {
-	var numArticles = doTheSearch();
-	setTimeout(function(){
-		response.render('index', {
-		  	results: numArticles
-		});
-	}, 1000)
-  
+	getOnePageArticles(formattedSearchVal, count, year, doneBoolean, function(numArticles, certainYearArticles, doneBoolean){
+		count = parseInt(count) + 1;
+		numArticlesTotal = parseInt(numArticlesTotal) + numArticles;
+		if (!doneBoolean){
+			response.redirect('/search/:' + count + '/:' + searchVal + '/:' + numArticlesTotal)
+		}
+		else{
+			response.render('index', {
+				numArticles: numArticlesTotal,
+				numArticlesYear: certainYearArticles.length,
+				numArticlesTotal: numArticlesTotal,
+				year: year,
+				searchVal: searchVal,
+				count: count
+			});
+		}
+	});
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+function getOnePageArticles(searchTerm, count, year, doneBoolean, callback){
+	var url = "http://www.ultiworld.com/";
+	console.log(url + "page/" + count + "/" + searchTerm)
+	request(url + "page/" + count + "/" + searchTerm, function(err, resp, body){
+
+		//Check for error
+	    if(err){
+	        return console.log('Error:', err);
+	    }
+
+	    //Check for right status code
+	    if(resp.statusCode !== 200){
+	        return console.log('Invalid Status Code Returned:', resp.statusCode);
+	    }
+
+		  $ = cheerio.load(body);
+		  var previousPageButton = $('.paging-nav__previous');			//see if there is a previous button, otherwise done searching
+		  if (!previousPageButton[0]){
+		  	doneBoolean = true;
+		  }			  
+		  var listElements = $('.snippet-excerpt__heading'); 		//list of articles
+		  var datesPosted = $('.snippet-excerpt__byline');			//date for each article
+		  var certainYearArticles = [];
+		  $(datesPosted).each(function(i, date){
+	  	  	var dateHTML = $(date).html();
+	  		var substring = dateHTML.substring(0,17).split(" ")[2]		//getting year of article
+	  		if (substring == '' + year){								//if year matches, add to this year article list
+	  			certainYearArticles.push($(listElements[i]).text());
+	  		}
+		  });
+		  callback(listElements.length, certainYearArticles, doneBoolean);
+	});
+}
 
 function formatSearchVal(searchVal){
 	var wordArray = searchVal.split(" ");
