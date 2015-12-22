36058923f33ea4072bd912f2996a51cb496efbcc
skultimate5 <sck42@case.edu>
2015-12-22 11:47:15 -0500

    Not working but previously had names of articles displayed
    Working on using qs to pass objects

--- cheerioTest.js
+++ cheerioTest.js
@@ -1,6 +1,7 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var express = require('express');
+var qs = require('qs');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -15,22 +16,47 @@ app.get('/', function(request, response) {
   response.render('index', {
   	numArticles: null,
   	numArticlesYear: null,
+  	certainYearArticles: [],
+  	certainYearArticlesLink: [],
+  	certainYearArticlesObj: null,
 	year: null,
 	searchVal: null,
 	count: 1,
-	numArticlesTotal: 0
+	numArticlesTotal: 0,
   });
 });
 
-app.get('/search/:count/:searchVal/:numArticlesTotal', function(req, response){
+//try saving info in database from search, and then loading data on completedSearch page
+//also have a loading screen which may need to be done this way
+
+app.get('/search/:count/:searchVal/:numArticlesTotal/:certainYearArticlesObj', function(req, response){
 	var count = req.params.count.split(":")[1];
-	console.log(req.params);
+	console.log(req.params.certainYearArticlesObj)
 	if (count == 1){
 		var searchVal = req.query.name;
+		var certainYearArticles = [];
+		var certainYearArticlesLink = [];
 	}
 	else{
 		var searchVal = req.params.searchVal.split(":")[1];
+		//var certainYearArticlesArray = req.params.certainYearArticles.split(":");
+		//var certainYearArticlesArrayLink = req.params.certainYearArticlesLink.split(":");
+		console.log("here")
+		console.log(qs.parse(req.params.certainYearArticlesObj));
+		var certainYearArticlesArray = certainYearArticlesObj.certainYearArticles;
+		var certainYearArticlesLink = certainYearArticlesObj.certainYearArticlesLink;
+		var certainYearArticles = [];
+		var certainYearArticlesLink = [];
+		for(var i = 1; i < certainYearArticlesArray.length; i++){
+			certainYearArticles.push(certainYearArticlesArray[i])
+			certainYearArticlesLink.push(certainYearArticlesArrayLink[i])
+		}
 	}
+	var certainYearArticlesObj = {
+		certainYearArticles: certainYearArticles,
+		certainYearArticlesLink: certainYearArticlesLink
+	};
+	console.log(certainYearArticlesObj)
 	var numArticlesTotal = req.params.numArticlesTotal.split(":")[1];
 	console.log("Search Value is: " + searchVal)
 	var formattedSearchVal = formatSearchVal(searchVal);
@@ -39,17 +65,24 @@ app.get('/search/:count/:searchVal/:numArticlesTotal', function(req, response){
 	var numArticlesYear = 0;
 	var doneBoolean = false;
 
-	getOnePageArticles(formattedSearchVal, count, year, doneBoolean, function(numArticles, certainYearArticles, doneBoolean){
+	getOnePageArticles(formattedSearchVal, count, year, doneBoolean, certainYearArticlesObj.certainYearArticles, certainYearArticlesObj.certainYearArticlesLink, function(numArticles, certainYearArticles, certainYearArticlesLink, doneBoolean){
+		var certainYearArticlesObj = {
+			certainYearArticles: certainYearArticles,
+			certainYearArticlesLink: certainYearArticlesLink
+		}
 		count = parseInt(count) + 1;
 		numArticlesTotal = parseInt(numArticlesTotal) + numArticles;
 		if (!doneBoolean){
-			response.redirect('/search/:' + count + '/:' + searchVal + '/:' + numArticlesTotal)
+			var query = qs.stringify(certainYearArticlesObj)
+			response.redirect('/search/:' + count + '/:' + searchVal + '/:' + numArticlesTotal + '/:' + query)
 		}
 		else{
 			response.render('index', {
 				numArticles: numArticlesTotal,
 				numArticlesYear: certainYearArticles.length,
 				numArticlesTotal: numArticlesTotal,
+				certainYearArticles: certainYearArticles,
+				certainYearArticlesLink: certainYearArticlesLink,
 				year: year,
 				searchVal: searchVal,
 				count: count
@@ -62,7 +95,7 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-function getOnePageArticles(searchTerm, count, year, doneBoolean, callback){
+function getOnePageArticles(searchTerm, count, year, doneBoolean, certainYearArticles, certainYearArticlesLink, callback){
 	var url = "http://www.ultiworld.com/";
 	console.log(url + "page/" + count + "/" + searchTerm)
 	request(url + "page/" + count + "/" + searchTerm, function(err, resp, body){
@@ -84,15 +117,16 @@ function getOnePageArticles(searchTerm, count, year, doneBoolean, callback){
 		  }			  
 		  var listElements = $('.snippet-excerpt__heading'); 		//list of articles
 		  var datesPosted = $('.snippet-excerpt__byline');			//date for each article
-		  var certainYearArticles = [];
 		  $(datesPosted).each(function(i, date){
 	  	  	var dateHTML = $(date).html();
 	  		var substring = dateHTML.substring(0,17).split(" ")[2]		//getting year of article
 	  		if (substring == '' + year){								//if year matches, add to this year article list
+	  			console.log($(listElements[i]).children().attr('href'));
 	  			certainYearArticles.push($(listElements[i]).text());
+	  			certainYearArticlesLink.push($(listElements[i]).children().attr('href'));
 	  		}
 		  });
-		  callback(listElements.length, certainYearArticles, doneBoolean);
+		  callback(listElements.length, certainYearArticles, certainYearArticlesLink, doneBoolean);
 	});
 }
 
